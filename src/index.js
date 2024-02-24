49a65a7082f0d31ed0def24092ea346b76b84eef
Trivera0713 <134112417+Trivera0713@users.noreply.github.com>
2024-02-24 06:17:17 -0500

    integrate AI Api

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
-function generatePoem(event) {
-  event.preventDefault();
-
+function displayRecipe(response) {
+  console.log(response.data.answer);
   new Typewriter("#recipe", {
-    strings: "Recipe will be here",
+    strings: response.data.answer,
     autoStart: true,
     delay: 1,
     cursor: "",
   });
+}
+
+function generatePoem(event) {
+  event.preventDefault();
+
+  let list = document.querySelector("#user-list");
+  let apiKey = "f68406t3o5c3f2a4369b987ab457dcba";
+  let prompt = `Generate a food recipe with the following ingredients ${list.value}`;
+  let context =
+    "You are renowned chef that has a versatile palate. Your mission is to generate 2 different food recipes in basic HTML. Make sure to use the ingredients provided by the user. ";
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  console.log(`prompt:${prompt}`);
+  console.log(`context:${context}`);
+  axios.get(apiUrl).then(displayRecipe);
 }
 
 let recipeForm = document.querySelector("#recipe-generator");
