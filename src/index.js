46cf6f50c74d328fb78f7a69cd280a03307bbfae
Trivera0713 <134112417+Trivera0713@users.noreply.github.com>
2025-02-22 00:05:10 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ function generateRecipe(event) {
   let apiKey = "f68406t3o5c3f2a4369b987ab457dcba";
   let prompt = `Generate a food recipe with the following ingredients ${list.value}`;
   let context =
-    "You are renowned chef that has a versatile palate. Your mission is to generate a food recipe in basic HTML. Make sure to only use the ingredients listed by the user. ";
+    "You are renowned chef that has a versatile palate. Your mission is to generate a food recipe in basic HTML. Make sure to only use the ingredients listed by the user, do not include page format in your response.";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
   let recipeElement = document.querySelector("#recipe");
@@ -23,7 +23,7 @@ function generateRecipe(event) {
   recipeElement.innerHTML = ` <div class="generating">
   Generating a delicious recipe for you with ${list.value}</div>`;
 
-  console.log(`prompt:${prompt}`);;
+  console.log(`prompt:${prompt}`);
   axios.get(apiUrl).then(displayRecipe);
 }
 
